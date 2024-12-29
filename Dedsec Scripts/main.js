021c13c3b707743c866ca30e1b8c1efdd739246e
sonic <156506053+sonic998@users.noreply.github.com>
2024-12-29 09:45:18 +0100

    Add files via upload

---
+++ Dedsec Scripts/main.js
@@ -0,0 +1,4 @@
+function copyscript()
+{
+	 navigator.clipboard.writeText("loadstring(game:HttpGet('https://sonic998.github.io/sonichub/SonicHub.lua'))()")
+}
---
+++ Sonic-Pics/sonicpics.js
@@ -0,0 +1,12 @@
+function Tab(container)
+{
+	document.getElementById("Sonic").className = "hide";
+	document.getElementById("Knuckles").className = "hide";
+	document.getElementById("Nine").className = "hide";
+	document.getElementById("Shadow").className = "hide";
+	document.getElementById("Silver").className = "hide";
+	document.getElementById("SonicPrime").className = "hide";
+	document.getElementById("SuperShadow").className = "hide";
+	document.getElementById("SuperSonic").className = "hide";
+    document.getElementById(container).className = "show";
+}
